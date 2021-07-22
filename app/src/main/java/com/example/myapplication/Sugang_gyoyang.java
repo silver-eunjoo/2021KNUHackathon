cea19d28a973c9a0a69c5c556fe602aaeac280d4
YHKim <80297525+kkyh12180@users.noreply.github.com>
2021-07-23 08:36:27 +0900

    Add files via upload

--- app/src/main/java/com/example/myapplication/Sugang_gyoyang.java
+++ app/src/main/java/com/example/myapplication/Sugang_gyoyang.java
@@ -17,13 +17,13 @@ public class Sugang_gyoyang {
 		gyoyang_name = new ArrayList<String>();
 		
 		//파일 경로 조정필요
-		String currentProjPath = "/Users/silver/AndroidStudioProjects";
+		String currentProjPath = "";
 		try {
 			currentProjPath = new File(".").getCanonicalPath();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		String currentFilePath = currentProjPath + "/new_gyoyang.txt";
+		String currentFilePath = currentProjPath + "/app/sugang/new_gyoyang.txt";
 		//
 		
 		Scanner inputStream = null;
