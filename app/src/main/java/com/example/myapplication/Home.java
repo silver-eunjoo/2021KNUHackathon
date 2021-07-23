30779e00ed6b09e1a9070e2c13afcf879bda269a
Leeeeunji <72423836+Leeeeunji@users.noreply.github.com>
2021-07-23 13:56:52 +0900

    Update Home.java

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
@@ -10,64 +13,55 @@ public class Home extends Fragment {
     private FragmentActivity myContext;
     public Button scheduleButton, postButton, advertisementButton;
 
+    public static Home newInstance() {
+        return new Home();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-}
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//
-//        View view =  inflater.inflate(R.layout.fragment_plan, container, false);
-//        scheduleButton=view.findViewById(R.id.scheduleButton);
-//        postButton=view.findViewById(R.id.postButton);
-//        advertisementButton=view.findViewById(R.id.advertisementButton);}
-//
-//        scheduleButton.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View view) {
-//
-//                FragmentManager fragmentManager = getFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentManager.replace(R.id.fragment, new Plan_Fragment());
-//                fragmentManager.commit();
-//            }
-//        });
-//
-//        postButton.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View view) {
-//
-//                FragmentManager fragmentManager = getFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentManager.replace(R.id.fragment, new CourseFragment());
-//                fragmentManager.commit();
-//            }
-//        });
-//
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        scheduleButton = (Button) view.findViewById(R.id.scheduleButton);
+        postButton = (Button) view.findViewById(R.id.postButton);
+        advertisementButton = (Button) view.findViewById(R.id.advertisementButton);
+
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+         //       ((MainActivity) getActivity()).replaceFragment(Plan.newInstance());
+
+            }
+        });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+           //     ((MainActivity) getActivity()).replaceFragment(CourseFragment.newInstance());
+            }
+        });
+
 //        advertisementButton.setOnClickListener(new View.OnClickListener(){
 //
 //            @Override
 //            public void onClick(View view) {
 //
-//                FragmentManager fragmentManager = getFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentManager.replace(R.id.fragment, new Plan_Fragment());
-//                fragmentManager.commit();
+//                ((MainActivity)getActivity()).replaceFragment(NewFragment.newInstance());
 //            }
 //        });
-//
-//
-//
-//
-//
-//
-//
-//    }
-//
-//}
+
+
+        return view;
+    }
+}
